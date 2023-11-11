f8d76257540d6932dcef2c54d36ef2217096e036
0xAF4 <karim.badalov1612@gmail.com>
2023-11-11 12:43:39 +0600

    jwt classes

--- index.go
+++ index.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-type InitMap map[string]interface{}
+type InitMap map[int]interface{}
 
 type TJWT struct {
 	algorithm       int
@@ -13,6 +13,13 @@ type TJWT struct {
 	accessDuration  time.Duration
 	refreshDuration time.Duration
 }
+
+const (
+	Algorithm = iota
+	SecretKey
+	AccessDuration
+	RefreshDuration
+)
 
 const (
 	HS256 = iota
@@ -30,20 +37,20 @@ func New(m *InitMap) (*TJWT, error) {
 	jwt := TJWT{}
 	var ok bool
 
-	if jwt.algorithm, ok = (*m)["algorithm"].(int); !ok {
-		return nil, errors.New("Укажите algorithm")
+	if jwt.algorithm, ok = (*m)[Algorithm].(int); !ok {
+		return nil, errors.New("Укажите Algorithm")
 	}
 
-	if jwt.secretKey, ok = (*m)["secretKey"].([]byte); !ok {
-		return nil, errors.New("Укажите accessToken")
+	if jwt.secretKey, ok = (*m)[SecretKey].([]byte); !ok {
+		return nil, errors.New("Укажите SecretKey")
 	}
 
-	if jwt.accessDuration, ok = (*m)["accessDuration"].(time.Duration); !ok {
-		return nil, errors.New("Укажите accessDuration")
+	if jwt.accessDuration, ok = (*m)[AccessDuration].(time.Duration); !ok {
+		return nil, errors.New("Укажите AccessDuration")
 	}
 
-	if jwt.refreshDuration, ok = (*m)["refreshDuration"].(time.Duration); !ok {
-		return nil, errors.New("Укажите refreshDuration")
+	if jwt.refreshDuration, ok = (*m)[RefreshDuration].(time.Duration); !ok {
+		return nil, errors.New("Укажите RefreshDuration")
 	}
 
 	return &jwt, nil
