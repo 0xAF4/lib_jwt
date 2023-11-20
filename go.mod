add9f2417e857efcb2bae45daa75d07e852809cc
0xAF4 <karim.badalov1612@gmail.com>
2023-11-20 22:02:44 +0600

    upd jwt

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/0xAF4/lib_jwt
 
 go 1.19
 
-require github.com/golang-jwt/jwt/v5 v5.1.0 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.1.0
+	github.com/google/uuid v1.4.0
+)
--- lib_jwt.go
+++ lib_jwt.go
@@ -5,9 +5,11 @@ import (
 	"time"
 
 	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
-type InitMap map[int]interface{}
+type JWTConfig map[int]interface{}
+type JWTClaim map[string]interface{}
 
 type TokenPair struct {
 	AccessToken  *string
@@ -15,15 +17,14 @@ type TokenPair struct {
 }
 
 type TJWT struct {
-	algorithm       int
+	method          jwt.SigningMethod
 	secretKey       []byte
 	accessDuration  time.Duration
 	refreshDuration time.Duration
-	token           *jwt.Token
 }
 
 const (
-	Algorithm = iota
+	Method = iota
 	SecretKey
 	AccessDuration
 	RefreshDuration
@@ -46,11 +47,14 @@ const (
 	PS512
 )
 
-func New(m *InitMap) (*TJWT, error) {
+func New(m *JWTConfig) (*TJWT, error) {
 	j := TJWT{}
-	var ok bool
+	var (
+		ok     bool
+		method int
+	)
 
-	if j.algorithm, ok = (*m)[Algorithm].(int); !ok {
+	if method, ok = (*m)[Method].(int); !ok {
 		return nil, fmt.Errorf("Укажите Algorithm")
 	}
 
@@ -66,95 +70,81 @@ func New(m *InitMap) (*TJWT, error) {
 		return nil, fmt.Errorf("Укажите RefreshDuration")
 	}
 
-	var method jwt.SigningMethod
-	switch j.algorithm {
+	switch method {
 	case HS256:
-		method = jwt.SigningMethodHS256
+		j.method = jwt.SigningMethodHS256
 	case HS384:
-		method = jwt.SigningMethodHS384
+		j.method = jwt.SigningMethodHS384
 	case HS512:
-		method = jwt.SigningMethodHS512
+		j.method = jwt.SigningMethodHS512
 	case ES256:
-		method = jwt.SigningMethodES256
+		j.method = jwt.SigningMethodES256
 	case ES384:
-		method = jwt.SigningMethodES384
+		j.method = jwt.SigningMethodES384
 	case ES512:
-		method = jwt.SigningMethodES512
+		j.method = jwt.SigningMethodES512
 	case EDDSA:
-		method = jwt.SigningMethodEdDSA
+		j.method = jwt.SigningMethodEdDSA
 	case NONE:
-		method = jwt.SigningMethodNone
+		j.method = jwt.SigningMethodNone
 	case RS256:
-		method = jwt.SigningMethodRS256
+		j.method = jwt.SigningMethodRS256
 	case RS384:
-		method = jwt.SigningMethodRS384
+		j.method = jwt.SigningMethodRS384
 	case RS512:
-		method = jwt.SigningMethodRS512
+		j.method = jwt.SigningMethodRS512
 	case PS256:
-		method = jwt.SigningMethodPS256
+		j.method = jwt.SigningMethodPS256
 	case PS384:
-		method = jwt.SigningMethodPS384
+		j.method = jwt.SigningMethodPS384
 	case PS512:
-		method = jwt.SigningMethodPS512
+		j.method = jwt.SigningMethodPS512
 	}
-
-	j.token = jwt.New(method)
 
 	return &j, nil
 }
 
-func (j *TJWT) GenerateTokenPair(claims map[string]interface{}) (*TokenPair, error) {
-	accessToken, err := j.generateAccessToken(claims)
+func (j *TJWT) GenerateTokenPair(claims *JWTClaim) (*TokenPair, error) {
+	UID := uuid.New()
+
+	(*claims)["uid"] = UID
+	(*claims)["sub"] = "access"
+	(*claims)["iat"] = time.Now().Unix()
+	(*claims)["exp"] = time.Now().Add(j.accessDuration).Unix()
+
+	accessToken, err := jwt.NewWithClaims(j.method, jwt.MapClaims(*claims)).SignedString(j.secretKey)
 	if err != nil {
 		return nil, fmt.Errorf("ошибка генерации AccessToken: %v", err)
 	}
 
-	refreshToken, err := j.generateRefreshToken(claims)
+	rClaim := &JWTClaim{
+		"uid": UID,
+		"sub": "refresh",
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(j.refreshDuration).Unix(),
+	}
+
+	refreshToken, err := jwt.NewWithClaims(j.method, jwt.MapClaims(*rClaim)).SignedString(j.secretKey)
 	if err != nil {
 		return nil, fmt.Errorf("ошибка генерации RefreshToken: %v", err)
 	}
 
 	return &TokenPair{
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
+		AccessToken:  &accessToken,
+		RefreshToken: &refreshToken,
 	}, nil
 }
 
-func (j *TJWT) generateAccessToken(claims map[string]interface{}) (*string, error) {
-	claims["exp"] = time.Now().Add(j.accessDuration).Unix()
-	//accesstoken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(claims))
-	//accessToken, err := accesstoken.SignedString(j.secretKey)
-	if err != nil {
-		return nil, err
-	}
-	return &accessToken, nil
-}
-
-/*
-func (j *TJWT) generateRefreshToken(claims map[string]interface{}) (*string, error) {
-	claims["exp"] = time.Now().Add(j.refreshDuration).Unix()
-	//refreshtoken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims(claims))
-	//refreshToken, err := refreshtoken.SignedString(j.secretKey)
-	if err != nil {
-		return nil, err
-	}
-	return &refreshToken, nil
-}
-*/
-/*
-func (j *TJWT) ParseToken(tokenString string) (jwt.MapClaims, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+func (j *TJWT) ParseToken(tokenString *string) (*JWTClaim, error) {
+	token, err := jwt.Parse(*tokenString, func(token *jwt.Token) (interface{}, error) {
 		return j.secretKey, nil
 	})
-	if err != nil {
-		return nil, err
-	}
 
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok || !token.Valid {
 		return nil, fmt.Errorf("invalid token")
 	}
 
-	return claims, nil
+	cl := JWTClaim(claims)
+	return &cl, err
 }
-*/
